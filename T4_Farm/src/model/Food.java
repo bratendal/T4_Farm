e95da3fc9654468bf78254e7b22b9b9e4efd8300
Per <Per@Per-TOSH>
2016-02-02 13:14:36 +0100

    Motherfucking animals

--- T4_Farm/src/model/Food.java
+++ T4_Farm/src/model/Food.java
@@ -5,6 +5,7 @@ public class Food {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("You can't Stump the Trump bajs");
+		System.out.println("Motherfucking animals!");
 	}
 
 }
